a179da9e6d5e03e30a5388bc306b2fbd7a994173
Abdirahman Ibrahim <ibrah342@umn.edu>
2025-03-12 18:10:44 +0900

    leetcode-api

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import requests 
+
+response = requests.get("https://alfa-leetcode-api.onrender.com/")
+
+print(response.status_code)#prints the status code to let us know if our coenncetion was succesful
+print(response.json())
+ 
